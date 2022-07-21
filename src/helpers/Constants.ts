affb1d086607ba6d5a16056fe5212b53e75759e7
godji83 <57142792+godji83@users.noreply.github.com>
2022-07-21 13:58:36 +0300

    Initial commit

---
+++ src/helpers/Constants.ts
@@ -0,0 +1,8 @@
+export class Constants {
+  public static stsAuthority = "https://localhost:44322/";
+  public static clientId = "BF8AD8F0-C907-4471-9A02-904667F4A5BF";
+  public static clientRoot = window.location.origin + "/"; //"http://localhost:4200/";
+  public static clientScope = "openid profile email api";
+
+  public static apiRoot = "https://localhost:44322/api/";
+}
